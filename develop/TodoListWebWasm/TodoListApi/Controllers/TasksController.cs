be788d4bca188568e7210090c3f5ebe6b8406ede
TranTienDe <sakurahonni@gmail.com>
2021-09-26 23:55:42 +0700

    + add CRUD TaskRepository

--- develop/TodoListWebWasm/TodoListApi/Controllers/TasksController.cs
+++ develop/TodoListWebWasm/TodoListApi/Controllers/TasksController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TodoListApi.Repositories;
+using Task = TodoListApi.Entities.Task;
 
 namespace TodoListApi.Controllers
 {
@@ -25,5 +26,44 @@ namespace TodoListApi.Controllers
             var tasks = await _repository.GetTaskList();
             return Ok(tasks);
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var task = await _repository.GetById(id);
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Task task)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var taskAdded = await _repository.Create(task);
+            return CreatedAtAction(nameof(GetById), new { id = taskAdded.Id }, taskAdded);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] Task task)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var taskFind = await _repository.GetById(id);
+            if (taskFind == null) return NotFound($"{id} is not found !");
+
+            var taskUpdated = await _repository.Update(taskFind);
+            return Ok(taskUpdated);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var taskFind = await _repository.GetById(id);
+            if (taskFind == null) return NotFound($"{id} is not found !");
+
+            var taskDeleted = await _repository.Delete(taskFind);
+            return Ok(taskDeleted);
+        }
+
     }
 }
