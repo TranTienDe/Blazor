a0f8373e14d2ac646bbd9019b29110198cd523a3
TranTienDe <sakurahonni@gmail.com>
2021-09-27 21:57:20 +0700

    + Add TaskCreate

---
+++ develop/TodoListWebWasm/TodoListWebWasm/Pages/TaskCreate.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Models;
+
+namespace TodoListWebWasm.Pages
+{
+    public partial class TaskCreate
+    {
+        private TaskCreateRequest Task = new TaskCreateRequest();
+
+        private void CreateForm(EditContext context)
+        {
+
+        }
+    }
+}
