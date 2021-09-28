2c6df96224081d81ee8242c89872da940249a496
TranTienDe <sakurahonni@gmail.com>
2021-09-28 10:00:24 +0700

    + Component communication

--- develop/TodoListWebWasm/TodoListWebWasm/Pages/TaskList.razor.cs
+++ develop/TodoListWebWasm/TodoListWebWasm/Pages/TaskList.razor.cs
@@ -15,20 +15,18 @@ namespace TodoListWebWasm.Pages
     public partial class TaskList
     {
         [Inject] private ITaskApiClient TaskApiClient { get; set; }
-        [Inject] private IUserApiClient UserApiClient { get; set; }
+
         [Inject] private IToastService ToastService { get; set; }
 
         private List<TaskDto> Tasks;
-        private List<AssigneeDto> Assignees;
         private TaskListSearch TaskListSearch = new TaskListSearch();
 
         protected override async Task OnInitializedAsync()
         {
             Tasks = await TaskApiClient.GetTaskList(TaskListSearch);
-            Assignees = await UserApiClient.GetUserList();
         }
 
-        private async Task SearchForm(EditContext context)
+        private async Task SearchForm(TaskListSearch taskListSearch)
         {
             /*ToastService.ShowInfo("Thông là thông báo !","Info");
             ToastService.ShowSuccess("Thông là thông báo !", "Info");
@@ -36,6 +34,7 @@ namespace TodoListWebWasm.Pages
             ToastService.ShowWarning("Thông là thông báo !", "Info");
             ToastService.ShowError("Thông là thông báo !", "Info");*/
 
+            TaskListSearch = taskListSearch;
             Tasks = await TaskApiClient.GetTaskList(TaskListSearch);
         }
     }
